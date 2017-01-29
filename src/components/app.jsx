bc2e1c05beea2a662060f3ac9be6e5ac2f8190f7
Chia-Lo (Caleb) Hsu <calebhsu@users.noreply.github.com>
2017-01-29 16:34:23 -0700

    Creates basic landing page for login (#16)
    * Reflexbox framework, landing to match mockup
    
    * Deletes unused config files
    
    * Set up and basic working react-router
    
    * Fixes hot loader, minor refactoring

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,14 @@
+import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import { green100, green500, green700 } from 'material-ui/styles/colors';
 
-import React from 'react';
-import NavBar from './NavBar';
+import Home from './Home';
 
 import '../scss/main.scss';
 
-const styles = {
-  container: {
-    marginTop: '15px',
-    textAlign: 'center',
-  },
-};
-
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: green500,
-    primary2Color: green700,
-    primary3Color: green100,
+    primary1Color: 'rgb(80, 17, 61)',
   },
 });
 
@@ -31,12 +20,7 @@ function App() {
   return (
     <MuiThemeProvider muiTheme={muiTheme}>
       <div>
-        <NavBar />
-        <div style={styles.container}>
-          <h1>It Works!</h1>
-          <p>This React project works including local CSS styles.</p>
-          <RaisedButton label="Enjoy" />
-        </div>
+        <Home />
       </div>
     </MuiThemeProvider>
   );
