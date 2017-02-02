3d0633176df2e7ae3be1269bfa06fc1594c4cb06
Caleb Hsu <chialo.hsu@colorado.edu>
2017-02-02 05:18:13 -0700

    Links to home from navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,8 @@ import AppBar from 'material-ui/AppBar';
 import Avatar from 'material-ui/Avatar';
 import Down from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
 import FlatButton from 'material-ui/FlatButton';
+import Home from 'material-ui/svg-icons/action/home';
+import IconButton from 'material-ui/IconButton';
 import ListItem from 'material-ui/List/ListItem';
 import { white } from 'material-ui/styles/colors';
 
@@ -57,7 +59,11 @@ function NavBar() {
         </Link>
       }
       iconStyleRight={styles.userElement}
-      showMenuIconButton={false}
+      iconElementLeft={
+        <Link to="/home">
+          <IconButton><Home color={white} /></IconButton>
+        </Link>
+      }
     />
   );
 }
