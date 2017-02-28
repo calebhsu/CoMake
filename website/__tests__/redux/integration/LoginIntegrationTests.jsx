6811ec12a194db933cf5deb5675ae328389b9b3b
Artur Meletyan <artm95@gmail.com>
2017-02-28 16:34:00 -0700

    added integration test for login

---
+++ website/__tests__/redux/integration/LoginIntegrationTests.jsx
@@ -0,0 +1,36 @@
+/**
+ * @file Integration test for Login Redux work flow.
+ */
+
+import storeHelper from '../../../src/redux/storeHelper';
+import * as LoginActions from '../../../src/redux/actions/LoginActions';
+import * as RC from '../../../src/redux/reducers/ReducerConstants';
+
+describe('LoginIntegrationTests', () => {
+
+  let testStore = null;
+
+  // create a new store before each test
+  beforeEach(() => {
+    testStore = storeHelper.constructStore();
+  });
+
+  // delete the old store after each test
+  afterEach(() => {
+    testStore = null;
+  });
+
+  test('updateUserInfo_dispatch', (done) => {
+    const testName = 'First Last';
+    const testURL = 'photoURL';
+    const expected = Object.assign({}, RC.BLANK_STATE);
+    expected[RC.USER_INFO][RC.USERNAME] = testName;
+    expected[RC.USER_INFO][RC.USER_PHOTO_URL] = testURL;
+    testStore.subscribe(() => {
+      expect(testStore.getState().LoginReducer).toEqual(expected);
+      done();
+    });
+
+    testStore.dispatch(LoginActions.updateUserInfo(testName, testURL));
+  });
+});
