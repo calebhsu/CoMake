213b479ba35a2be86656c631f6c9eb6b931f31df
Artur Meletyan <artm95@gmail.com>
2017-02-28 17:27:56 -0700

    added reducer util functions to LoginReducer.jsx

--- website/src/redux/reducers/LoginReducer.jsx
+++ website/src/redux/reducers/LoginReducer.jsx
@@ -4,20 +4,17 @@
 
 import { UPDATE_USER_INFO } from './../actions/ActionConstants';
 
+import * as RC from './ReducerConstants';
+import { insertIntoState } from './ReducerUtil';
+
  /**
   * Update the position in firebase.
   * @param {Object} state The state of the store.
   * @param {Object} action action to be performed.
   * @returns {Object} The new state object.
   */
- export const userInfoReducer = (state = { elements: {} }, action) => {
-   switch (action.type) {
-     case UPDATE_USER_INFO:
-      const newState = Object.assign({}, state, {
-        userInfo: action.payload
-      });
-      return newState;
-     default:
-      return state;
-   }
- };
+export const userInfoReducer = (state = RC.BLANK_STATE, action) => {
+  if(action.type === UPDATE_USER_INFO) {
+    return insertIntoState(state, action.payload, [RC.USER_INFO])
+  } else return state;
+};
