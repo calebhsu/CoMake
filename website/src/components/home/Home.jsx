e670b31442972d3148d6e769ddb4ab0635cdec13
Sean Hansberry <seanhansberry@users.noreply.github.com>
2017-03-02 00:26:51 -0700

    Refactors Home page to add unit test structure and verify canvas listings/resizing (#77)
    * Updated model display on homepage for easier unit testing
    
    * Cleaned up code / Changed models to canvases
    
    * Added JSDoc comments
    
    * Fixed models to canvases
    
    * Added unit test framework and updated the canvas thumbnail to resize according to the number of canvases on the users list
    
    * Fixed JSDoc syntax
    
    * Moved canvas list creation to new file
    
    * npm test HomeTests now runs Home page unit tests (mock version)
    
    * npm HomeTests now checks if CanvasList is defined
    
    * Refactored Home page
    
    * Fixed syntax and file structure for pull request comments
    
    * Fixed syntax errors

--- website/src/components/home/Home.jsx
+++ website/src/components/home/Home.jsx
@@ -1,29 +1,18 @@
+/**
+ * @file HTML generation for the Home page
+ */
+
 import React from 'react';
 import { Box, Flex } from 'reflexbox';
-import { Link } from 'react-router';
-
-import { Card, CardHeader, CardMedia } from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
-
-import CoMakeServices from 'comake-services';
-
-import ServiceEndpoint from '../../ServiceEndpoint'
-
-const CanvasCreationService = CoMakeServices.CanvasCreationService;
+import CanvasList from './CanvasList';
+import CreateCanvas from './CreateCanvas';
 
 const styles = {
   body: {
     textAlign: 'center',
   },
-  img: {
-    height: 'auto',
-    width: 100,
-  },
   header: {
     textAlign: 'center',
-  },
-  overlay: {
-    padding: 0,
   },
   welcome: {
     fontSize: '3.5em',
@@ -32,39 +21,11 @@ const styles = {
   },
 };
 
-function createNewCanvas(){
-  const reqBody = CanvasCreationService.formRequestBody(
-    'new canvas',
-    '0',
-    '1',
-    ['0']
-  );
-
-  CanvasCreationService.sendRequest(reqBody, ServiceEndpoint, () => {});
-}
-
 /**
  * Gives HTML for the home page after login.
- * @returns {HTML}   The HTML of the home page.
+ * @returns {HTML} The HTML of the home page.
  */
 function Home() {
-  let models = [];
-  for( let i = 0; i < 5; i++ ){
-    models.push(
-      <Box col={2} sm={4} md={2} key={i}>
-        <Link to="/canvas">
-          <Card>
-            <CardMedia
-              overlay={<CardHeader title="Racecar" />}
-              overlayContentStyle={styles.overlay}
-            >
-              <img src="https://res.cloudinary.com/craftml/image/upload/w_250,h_250,c_fill/v1440024165/4yUaf.png" style={styles.img} />
-            </CardMedia>
-          </Card>
-        </Link>
-      </Box>
-    )
-  }
   return (
     <Flex
       align="center"
@@ -78,22 +39,15 @@ function Home() {
       <Box col={12} sm={12} mb={4}>
         <header style={styles.header}>
           <h1 style={styles.welcome}>Welcome to Comake</h1>
-          <span>
-            First time user? &nbsp;&nbsp;
-            <Link to="/canvas">
-              <RaisedButton
-                label="Create New Model"
-                onClick={createNewCanvas}
-                secondary={true}
-              />
-            </Link>
-          </span>
-        </header>
+          <CreateCanvas />
+          </header>
       </Box>
       <Box col={12} sm={12}>
         <p>Pick up where you left off.</p>
       </Box>
-      {models}
+      <div>
+        <CanvasList />
+      </div>
     </Flex>
   )
 }
