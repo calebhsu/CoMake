be5a97cc268c349828924513ba411b41984afa24
Artur Meletyan <artm95@gmail.com>
2017-03-06 18:16:23 -0700

    added necessary action constants

--- website/src/redux/actions/ActionConstants.jsx
+++ website/src/redux/actions/ActionConstants.jsx
@@ -15,8 +15,12 @@ export const TARGET_ELEMENT = 'targetElement';
 
 // Canvas Actions
 export const ADD_CANVAS = 'addCanvas';
+export const ADD_CANVAS_USER = 'addCanvasUser';
 export const REMOVE_CANVAS = 'removeCanvas';
+export const REMOVE_CANVAS_USER = 'removeCanvasUser';
 export const SET_CURRENT_CANVAS = 'setCurrentCanvas';
+export const SET_CANVAS_NAME = 'setCanvasName';
+export const SET_CANVAS_OWNER = 'setCanvasOwner';
 
 // User Actions
 export const UPDATE_USER_INFO = 'updateUserInfo';
