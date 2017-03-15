69ed5d0326e3fb8782b2cd0472592dc7eb5185d1
Caleb Hsu <chialo.hsu@colorado.edu>
2017-03-15 01:36:20 -0600

    Updates tests

--- backend/client-pkg/__tests__/services/integration/ModelImportServiceIntegrationTests.js
+++ backend/client-pkg/__tests__/services/integration/ModelImportServiceIntegrationTests.js
@@ -2,16 +2,13 @@
  * @file Integration tests for the ModelImportService client package
  */
 
-const ModelImportService = require('../../../services/CanvasSharingService');
+const ModelImportService = require('../../../services/ModelImportService');
 
 describe('ModelImportServiceIntegrationTests', () => {
 
   test('sendRequest_requestReturns', () => {
-    const requestBody = ModelImportService.formRequestBody(
-      '9dKZ3'
-    );
     const expectedRequest = 'http://res.cloudinary.com/craftml/image/upload/v1489381041/9dKZ3/-Kf50m7ppARLoDtW5-kR.png'
-    const requestResp = ModelImportService.sendRequest(requestBody);
+    const requestResp = ModelImportService.sendRequest('9dKZ3');
     expect(requestResp).toEqual(expectedRequest);
   });
 
--- backend/client-pkg/__tests__/services/unit/ModelImportServiceUnitTests.js
+++ backend/client-pkg/__tests__/services/unit/ModelImportServiceUnitTests.js
@@ -4,23 +4,13 @@
 const ModelImportService = require('../../../services/ModelImportService');
 
 describe('ModelImportServiceUnitTests', () => {
-  test('formRequest_success', () => {
-    const modelId = '9dKZ3';
-
-    const expectedRequest = modelId;
-
-    const actualRequest = ModelImportService.formRequestBody(modelId);
-
-    expect(actualRequest).toEqual(expectedRequest);
-  });
-
-  test('formRequest_nameInvalid_exception', () => {
+  test('sendRequest_nameInvalid_exception', () => {
     const modelId = 92315;
 
     try {
-      ModelImportService.formRequestBody(modelId);
+      ModelImportService.sendRequest(modelId);
     } catch(err) {
-      expect(err).toEqual('ModelImportService.formRequestBody - invalid modelId param, must be a String');
+      expect(err).toEqual('ModelImportService.sendRequest - invalid modelId param, must be a string');
     }
   });
 });
