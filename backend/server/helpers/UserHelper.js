d0f3b870c5b4d5938048490b8a9623d174857f7c
sperry94 <sepe4375@colorado.edu>
2017-03-19 21:21:51 -0600

    returning an array in the ShareCanvasService response to indicate which users in the list were not found

--- backend/server/helpers/UserHelper.js
+++ backend/server/helpers/UserHelper.js
@@ -5,10 +5,12 @@
  const admin = require('firebase-admin');
  const winston = require('winston');
 
+ const Errors = require('./Errors');
+
  /**
   * Looks up a firebase user by their email
   * @param {string} email the email of the user to lookup
-  * @returns {Promise} A promise that contains a Firebase DataSnapshot with the relevant user
+  * @returns {Promise} A promise that contains a Firebase DataSnapshot with an array of the query results
   */
  const getFirebaseUserByEmail = (email) => {
   winston.info(
@@ -18,9 +20,8 @@
 
   const usersRef = admin.database().ref('/users');
   try {
-    // TODO: figure out how to notify if email wasn't found
     return usersRef.orderByChild('email').equalTo(email).limitToFirst(1)
-      .once('child_added').catch((error) => {
+      .once('value').catch((error) => {
         winston.error(
           'UserHelper.getFirebaseUserByEmail - Error looking up user by email %s. Error message: %s',
           email,
@@ -41,6 +42,7 @@
  * Adds a user specified by email to the canvas designated by canvasId
  * @param {string} email the email of the user to add the the canvas
  * @param {string} canvasId the canvas id of the canvas to add the user to
+ * @throws When user is not found in DB
  * @returns {void}
 */
 const addUserToCanvasByEmail = (email, canvasId) => {
@@ -50,82 +52,92 @@ const addUserToCanvasByEmail = (email, canvasId) => {
     canvasId
   );
 
-  getFirebaseUserByEmail(email).then((userSnap) => {
+  return getFirebaseUserByEmail(email).then((queryResultSnap) => {
+    if(!(queryResultSnap.val()) || queryResultSnap.numChildren() !== 1) {
+      winston.info(
+        'UserHelper.addUserToCanvasByEmail - no info for user %s found in the database %s',
+        email
+      );
+      throw Errors.UserNotFound;
+    }
+
     const canvasRef = admin.database().ref('/canvases/' + canvasId);
 
-    try {
-      // add user to the canvas's user list
-      canvasRef.child('/users').transaction((oldValue) => {
-        winston.info(
-          'UserHelper.addUserToCanvasByEmail - writing user %s to canvas %s',
-          email,
-          canvasId
-        );
-        if(!oldValue)
-          oldValue = {};
-        oldValue[userSnap.key] = userSnap.child('email').val();
-        return oldValue;
-      }).then(() => {
-        winston.info(
-          'UserHelper.addUserToCanvasByEmail - successfully added user %s to canvas %s',
-          email,
-          canvasId
-        );
-      }).catch((error) => {
+    queryResultSnap.forEach((userSnap) => {
+      try {
+        // add user to the canvas's user list
+        canvasRef.child('/users').transaction((oldValue) => {
+          winston.info(
+            'UserHelper.addUserToCanvasByEmail - writing user %s to canvas %s',
+            email,
+            canvasId
+          );
+          if(!oldValue)
+            oldValue = {};
+          oldValue[userSnap.key] = userSnap.child('email').val();
+          return oldValue;
+        }).then(() => {
+          winston.info(
+            'UserHelper.addUserToCanvasByEmail - successfully added user %s to canvas %s',
+            email,
+            canvasId
+          );
+        }).catch((error) => {
+          winston.error(
+            'UserHelper.addUserToCanvasByEmail - error adding user %s to canvas %s: %s',
+            email,
+            canvasId,
+            error.message
+          );
+        });
+      } catch (error) {
         winston.error(
           'UserHelper.addUserToCanvasByEmail - error adding user %s to canvas %s: %s',
           email,
           canvasId,
           error.message
         );
-      });
-    } catch (error) {
-      winston.error(
-        'UserHelper.addUserToCanvasByEmail - error adding user %s to canvas %s: %s',
-        email,
-        canvasId,
-        error.message
-      );
-    }
+      }
 
-    try {
-      // add canvas to the user's canvas list
-      canvasRef.child('name').once('value').then((canvasNameSnap) => {
-        admin.database().ref('/users/' + userSnap.key + '/canvases')
-          .transaction((oldValue) => {
-            winston.info(
-              'UserHelper.addUserToCanvasByEmail - writing canvas %s to user %s',
-              canvasId,
-              email
-            );
+      try {
+        // add canvas to the user's canvas list
+        canvasRef.child('name').once('value').then((canvasNameSnap) => {
+          admin.database().ref('/users/' + userSnap.key + '/canvases')
+            .transaction((oldValue) => {
+              winston.info(
+                'UserHelper.addUserToCanvasByEmail - writing canvas %s to user %s',
+                canvasId,
+                email
+              );
 
-            if(!oldValue)
-              oldValue = {};
-            oldValue[canvasId] = canvasNameSnap.val();
-            return oldValue;
-          }).then(() => {
-            winston.info(
-              'UserHelper.addUserToCanvasByEmail - successfully added canvas %s to user %s',
-              canvasId,
-              email
-            );
-          }).catch((error) => {
-            winston.error(
-              'UserHelper.addUserToCanvasByEmail - error adding canvas %s to user %s: %s',
-              canvasId,
-              email,
-              error.message
-            );
-          });
-      });
-    } catch (error) {
-      winston.error(
-        'UserHelper.addUserToCanvasByEmail - error adding canvas %s to user %s: %s',
-        canvasId,
-        email,
-        error.message
-      );
-    }
+              if(!oldValue)
+                oldValue = {};
+              oldValue[canvasId] = canvasNameSnap.val();
+              return oldValue;
+            }).then(() => {
+              winston.info(
+                'UserHelper.addUserToCanvasByEmail - successfully added canvas %s to user %s',
+                canvasId,
+                email
+              );
+            }).catch((error) => {
+              winston.error(
+                'UserHelper.addUserToCanvasByEmail - error adding canvas %s to user %s: %s',
+                canvasId,
+                email,
+                error.message
+              );
+            });
+        });
+      } catch (error) {
+        winston.error(
+          'UserHelper.addUserToCanvasByEmail - error adding canvas %s to user %s: %s',
+          canvasId,
+          email,
+          error.message
+        );
+      }
+    });
   });
  };
 
