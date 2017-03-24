a152352a4c2683b97815944983a5965575760816
Zach Lamb <zala3440@colorado.edu>
2017-03-24 09:37:01 -0600

    atom beautify

--- website/src/components/canvas/options-bar/OptionsBar.jsx
+++ website/src/components/canvas/options-bar/OptionsBar.jsx
@@ -1,8 +1,8 @@
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import * as firebase from 'firebase';
 
-import { Box } from 'reflexbox';
+import {Box} from 'reflexbox';
 import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
@@ -16,173 +16,144 @@ import ShareCanvasModal from './ShareCanvasModal';
 import * as RC from '../../../redux/reducers/ReducerConstants';
 import * as CA from '../../../redux/actions/CanvasActions';
 
-
-import {
-  white,
-  purple500,
-  blue500,
-  green400,
-  orange500,
-} from 'material-ui/styles/colors';
+import {white, purple500, blue500, green400, orange500} from 'material-ui/styles/colors';
 
 const COLORS = [purple500, blue500, green400, orange500];
 
 const styles = {
-  box: {
-    width: '100%'
-  },
-  header: {
-    backgroundColor: '#49937f',
-    color: '#FFFFFF',
-    marginTop: 0,
-    padding: '15px 10px',
-    textTransform: 'uppercase',
-  },
-  modelName: {
-    float: 'left',
-    marginLeft: 15,
-  },
-  optionBtn: {
-    marginTop: 10,
-  },
-  optionBtnGroup: {
-    float: 'left',
-    marginLeft: 20,
-    marginRight: 10,
-  },
-  paper: {
-    display: 'inline-block',
-    height: 50,
-    textAlign: 'center',
-    width: '100%',
-  }
+    box: {
+        width: '100%'
+    },
+    header: {
+        backgroundColor: '#49937f',
+        color: '#FFFFFF',
+        marginTop: 0,
+        padding: '15px 10px',
+        textTransform: 'uppercase'
+    },
+    modelName: {
+        float: 'left',
+        marginLeft: 15
+    },
+    optionBtn: {
+        marginTop: 10
+    },
+    optionBtnGroup: {
+        float: 'left',
+        marginLeft: 20,
+        marginRight: 10
+    },
+    paper: {
+        display: 'inline-block',
+        height: 50,
+        textAlign: 'center',
+        width: '100%'
+    }
 };
 
 class OptionsBar extends React.Component {
-  /**
+    /**
    * Constructor for the class.
    * @param {Object} props The props passed to the component.
    * @returns {void}
    */
-  constructor(props) {
-    super(props);
-    this.state = {
-      snackbarOpen: false,
-    };
-    this.nameFieldChangeHandler = this.nameFieldChangeHandler.bind(this);
-    this.handleSnackbarRequestClose = this.handleSnackbarRequestClose.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            snackbarOpen: false
+        };
+        this.nameFieldChangeHandler = this.nameFieldChangeHandler.bind(this);
+        this.handleSnackbarRequestClose = this.handleSnackbarRequestClose.bind(this);
+    }
 
-  /**
+    /**
    * Function to automatically be performed once the component mounts.
    * @returns {void}
    */
-  componentDidMount() {
-    const canvasPath = 'canvases/' + this.props.currentCanvas + '/';
-    firebase.database().ref(canvasPath + RC.CANVAS_NAME).on('value', (snap) => {
-      this.props.dispatch(CA.setCanvasName(this.props.currentCanvas, snap.val()));
-    });
-    firebase.database().ref(canvasPath + RC.CANVAS_USERS).on('child_added', (snap) => {
-      this.props.dispatch(CA.addCanvasUser(this.props.currentCanvas, snap.key, snap.val()));
-    })
-  }
+    componentDidMount() {
+        const canvasPath = 'canvases/' + this.props.currentCanvas + '/';
+        firebase.database().ref(canvasPath + RC.CANVAS_NAME).on('value', (snap) => {
+            this.props.dispatch(CA.setCanvasName(this.props.currentCanvas, snap.val()));
+        });
+        firebase.database().ref(canvasPath + RC.CANVAS_USERS).on('child_added', (snap) => {
+            this.props.dispatch(CA.addCanvasUser(this.props.currentCanvas, snap.key, snap.val()));
+        })
+    }
 
-  /**
+    /**
    * After we unmount the canvas stop listening to the elements.
    * @returns {void}
    */
-  componentWillUnmount() {
-    const canvasPath = 'canvases/' + this.props.currentCanvas + '/';
-    firebase.database().ref(canvasPath).off();
-  }
-  /**
+    componentWillUnmount() {
+        const canvasPath = 'canvases/' + this.props.currentCanvas + '/';
+        firebase.database().ref(canvasPath).off();
+    }
+    /**
    * Handler for onRequestClose that sets snackbar's open state to false.
    * @returns {void}
    */
-  handleSnackbarRequestClose() {
-    this.setState({snackbarOpen: false});
-  }
-  /**
+    handleSnackbarRequestClose() {
+        this.setState({snackbarOpen: false});
+    }
+    /**
    * Handler for when the name field is changed.
    * @param {Object} e  The event of changing the name.
    * @param {String} newValue The new name entered.
    * @returns {void}
    */
-  nameFieldChangeHandler(e, newValue) {
-    this.props.dispatch(CA.setCanvasNameAndPersist(this.props.currentCanvas,
-      newValue))
-      this.setState({snackbarOpen: true});
-  }
+    nameFieldChangeHandler(e, newValue) {
+        this.props.dispatch(CA.setCanvasNameAndPersist(this.props.currentCanvas, newValue))
+        this.setState({snackbarOpen: true});
+    }
 
-  /**
+    /**
    * Gives HTML for the canvas options bar.
    * @returns {HTML}   The HTML of the canvas options toolbar.
    */
-  render() {
-    let canvasName = 'Canvas Name';
-    const userDivs = [];
-    if (this.props.canvas) {
-      canvasName = this.props.canvas[RC.CANVAS_NAME];
-      if (this.props.canvas[RC.CANVAS_USERS]) {
-        let currColorIndex = 0;
-        Object.keys(this.props.canvas[RC.CANVAS_USERS]).forEach((uid) => {
-          const userEmail = this.props.canvas[RC.CANVAS_USERS][uid];
-          userDivs.push(
-            <IconButton
-              tooltip={userEmail}
-              touch={true}
-              tooltipPosition="bottom-center"
-              key={userEmail}
-            >
-              <Avatar
-                color={white}
-                backgroundColor={COLORS[currColorIndex]}
-                style={styles.avatar}
-                size={30}
-              >
-                {userEmail[0]}
-              </Avatar>
-            </IconButton>
-          );
-          currColorIndex = (currColorIndex + 1) % COLORS.length;
-        });
-      }
+    render() {
+        let canvasName = 'Canvas Name';
+        const userDivs = [];
+        if (this.props.canvas) {
+            canvasName = this.props.canvas[RC.CANVAS_NAME];
+            if (this.props.canvas[RC.CANVAS_USERS]) {
+                let currColorIndex = 0;
+                Object.keys(this.props.canvas[RC.CANVAS_USERS]).forEach((uid) => {
+                    const userEmail = this.props.canvas[RC.CANVAS_USERS][uid];
+                    userDivs.push(
+                        <IconButton tooltip={userEmail} touch={true} tooltipPosition="bottom-center" key={userEmail}>
+                            <Avatar color={white} backgroundColor={COLORS[currColorIndex]} style={styles.avatar} size={30}>
+                                {userEmail[0]}
+                            </Avatar>
+                        </IconButton>
+                    );
+                    currColorIndex = (currColorIndex + 1) % COLORS.length;
+                });
+            }
+        }
+        return (
+            <div>
+                <Box style={styles.box} col={9} sm={12} md={9}>
+                    <Paper style={styles.paper} zDepth={1}>
+                        <TextField style={styles.modelName} id="text-field-default" defaultValue={canvasName} onChange={this.nameFieldChangeHandler}/>
+                        <span style={styles.optionBtnGroup}>
+                            <FlatButton label="File Options" style={styles.optionBtn}/>
+                            <ImportModelModal currentCanvas={this.props.currentCanvas}/>
+                            <ExportModal elements={this.props.elements}/>
+                            <ShareCanvasModal/> {userDivs}
+                        </span>
+                    </Paper>
+                </Box>
+                <Snackbar autoHideDuration={2000} message="Name change saved" onRequestClose={this.handleSnackbarRequestClose} open={this.state.snackbarOpen}/>
+            </div>
+        );
     }
-    return (
-      <div>
-        <Box style={styles.box} col={9} sm={12} md={9}>
-          <Paper style={styles.paper} zDepth={1}>
-            <TextField
-              style={styles.modelName}
-              id="text-field-default"
-              defaultValue={canvasName}
-              onChange={this.nameFieldChangeHandler}
-            />
-            <span style={styles.optionBtnGroup}>
-              <FlatButton label="File Options" style={styles.optionBtn} />
-              <ImportModelModal currentCanvas={this.props.currentCanvas} />
-              <ExportModal elements={this.props.elements} />
-              <ShareCanvasModal />
-              { userDivs }
-            </span>
-          </Paper>
-        </Box>
-        <Snackbar
-          autoHideDuration={2000}
-          message="Name change saved"
-          onRequestClose={this.handleSnackbarRequestClose}
-          open={this.state.snackbarOpen}
-        />
-      </div>
-    );
-  }
 }
 
 OptionsBar.propTypes = {
-  dispatch: PropTypes.func,
-  elements: PropTypes.object,
-  canvas: PropTypes.object,
-  currentCanvas: PropTypes.string,
+    dispatch: PropTypes.func,
+    elements: PropTypes.object,
+    canvas: PropTypes.object,
+    currentCanvas: PropTypes.string
 }
 
 export default connect()(OptionsBar);
