371ea6f33df4b1b33cea8eddae72058ed06874c4
Artur Meletyan <artm95@gmail.com>
2017-04-07 18:19:56 -0600

    reverted store helper file

--- website/src/redux/storeHelper.jsx
+++ website/src/redux/storeHelper.jsx
@@ -1,23 +1,27 @@
 /**
  * @file File to compile redux store helpers.
  */
+
 import { applyMiddleware, compose, createStore } from 'redux';
 import createActionBuffer from 'redux-action-buffer';
 import { autoRehydrate } from 'redux-persist';
 import { REHYDRATE } from 'redux-persist/constants';
 import thunkMiddleware from 'redux-thunk';
+
 import reducers from './reducers';
+
 /**
  * Constructs the redux store and persists if specified
  * @param {Bool} persist whether or not to persist/rehyrdate the store
  * @returns {Store} A redux store with the reducers from the reducers folder and the thunk middleware applied to it.
  */
 const storeConstructor = (persist) => {
+
   let enhancers = null
-  const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
   if(persist) {
     //will persist
-    enhancers = composer(
+    enhancers = compose(
       autoRehydrate(),
       applyMiddleware(
         thunkMiddleware,
@@ -31,12 +35,14 @@ const storeConstructor = (persist) => {
         thunkMiddleware
     );
   }
+
   return createStore(
     reducers,
     {},
     enhancers
   );
 };
+
 export default {
   constructStore: storeConstructor
 }
