f9d04e03550bb7531cffd61ad5540f51e74e2ef8
Ian <ianchar4@gmail.com>
2017-04-26 00:07:03 -0600

    Duplicate model (#213)
    * made canvasId appear in url during canvas modification
    
    * fixed issue where creating canvases would not redirect to new canvas page
    
    * Canvas now reads off of url
    
    * Initial url loading
    
    * Removed current canvas from redux
    
    * Added canvas loadding and error pages
    
    * Added 404 page
    
    * Canvas changes on url change
    
    * Updated comments
    
    * Changed add model to duplicate
    
    * Cloned model now has same properties

--- website/src/components/canvas/CanvasConstants.jsx
+++ website/src/components/canvas/CanvasConstants.jsx
@@ -11,12 +11,12 @@ export const IMAGE_UPLOAD_ERROR = 'Error uploading canvas image.';
 export const IMAGE_SAVE_MESSAGE = 'Image Saved';
 
 // Sidebar buttons
-export const ADD_ELEMENT_BUTTON = 'Add Model';
+export const CLONE_ELEMENT_BUTTON = 'Clone Current Model';
 export const DELETE_ELEMENT_BUTTON = 'Delete Current Model';
 export const RENDER_BUTTON = 'Render';
 export const CLEAR_3D_BUTTON = 'Clear 3D Preview';
 export const SAVE_3D_IMAGE_BUTTON = 'Save Rendered Image';
-export const SIDEBAR_BUTTONS = [ADD_ELEMENT_BUTTON, DELETE_ELEMENT_BUTTON];
+export const SIDEBAR_BUTTONS = [CLONE_ELEMENT_BUTTON, DELETE_ELEMENT_BUTTON];
 export const AUTO_RENDER_CHECKBOX = 'Auto Render';
 export const AUTO_RENDER_LABEL_POSITION = 'left';
 
